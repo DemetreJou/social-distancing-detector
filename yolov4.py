4bec3a08d778e5cf085e86dac5fff874ee846a88
DemetreJou <demetre.jouras@gmail.com>
2020-12-07 22:59:22 -0500

    add framework for checking social distancing
    added Person class
    need to implement set_social_distancing function

--- yolov4.py
+++ yolov4.py
@@ -6,6 +6,9 @@ CONFIDENCE_THRESHOLD = 0.2
 NMS_THRESHOLD = 0.4
 COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
 
+green = (0, 255, 0)
+red = (0, 0, 255)
+
 with open("./darknet/classes.txt", "r") as f:
     class_names = [cname.strip() for cname in f.readlines()]
 
@@ -27,8 +30,43 @@ net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
 model = cv2.dnn_DetectionModel(net)
 model.setInputParams(size=(416, 416), scale=1/255)
 
+
+class Person:
+    score: int
+    box: list
+    social_distancing: bool
+
+    def __init__(self, score, box):
+        self.score = score
+        self.box = box
+        self.social_distancing = True
+
+    def __eq__(self, other):
+        for a, b in zip(self.box, other.box):
+            if a != b:
+                return False
+        return True
+
+    @property
+    def middle(self):
+        return (self.box[0] + self.box[2]//2, self.box[1] + self.box[3]//2)
+
+    @property
+    def width(self):
+        return self.box[1]
+
+    @property
+    def height(self):
+        return self.box[0]
+
+    def set_social_distancing(self, other):
+        # TODO: implement
+        pass
+
+
 frame_number = 0
 while vc.isOpened():
+    detected_people = []
     (grabbed, frame) = vc.read()
     if not grabbed:
         print("not grabbed")
@@ -40,14 +78,23 @@ while vc.isOpened():
 
     start_drawing = time.time()
     for (classid, score, box) in zip(classes, scores, boxes):
-        color = COLORS[int(classid) % len(COLORS)]
-        label = "%s : %f" % (class_names[classid[0]], score)
-        cv2.rectangle(frame, box, color, 2)
-        cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        # color = COLORS[int(classid) % len(COLORS)]
+        detected_class = class_names[classid[0]]
+        if detected_class == "person":
+            detected_people.append(Person(score, box))
 
-        # TODO: add function to find center of person
+    # check for social distancing
+    for person in detected_people:
+        for other in detected_people:
+            if person != other:
+                person.set_social_distancing(other)
 
-    # TODO: find distance between all pairs of detected people
+    for person in detected_people:
+        label = "%s : %f" % ("person", person.score)
+        color = green if person.social_distancing else red
+        cv2.rectangle(frame, person.box, color, 2)
+        # (box[0], box[1] - 10)
+        cv2.putText(frame, label, person.middle, cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
     end_drawing = time.time()
     
