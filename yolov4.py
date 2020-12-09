0ef523599d7719dcd3c1a19545f2a6cf33d8f657
Mark Abdullah <markabdullah321@gmail.com>
2020-12-09 11:15:27 -0500

    Code cleanup, organiize and properly comment

--- yolov4.py
+++ yolov4.py
@@ -2,125 +2,56 @@ import numpy as np
 import cv2
 import time
 import os
+from person import Person
 
+###### Model Detection Parameters ######
 CONFIDENCE_THRESHOLD = 0.2
 NMS_THRESHOLD = 0.4
-COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
 
+###### Drawing shapes variables and colours ######
 green = (0, 255, 0)
 red = (0, 0, 255)
 social_distancing_min_distance = 200
 
+###### Get the list of trained classes our model config/weights file ######
 with open("./darknet/classes.txt", "r") as f:
     class_names = [cname.strip() for cname in f.readlines()]
 
-# vc = cv2.VideoCapture("http://192.168.2.60:4747/video")  # TODO: for droid cam
 
-file_name = "EnterExitCrossingPaths2front_10fps"  # to help with saving later
+###### Choose the video we want to run on ######
+file_name, fps = "EnterExitCrossingPaths2front_10fps", 10
+# file_name, fps = "EnterExitCrossingPaths2front_shortened", 25
 extention = "mp4"
-fps=10
+
+###### Setup the relative input/output folders ######
 relative_path = f"./Videos/{file_name}.{extention}"
 output_folder = "Output"
-
 os.makedirs(output_folder, exist_ok=True)
 
+###### Setup the video capture and video writer objects ######
 vc = cv2.VideoCapture(relative_path)
+(grabbed, frame) = vc.read() # Steal the first frame to get the size hahaha 
+height, width = frame.shape[0], frame.shape[1]
+fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+video_writer = cv2.VideoWriter(f'./{output_folder}/{file_name}_detection.avi', fourcc, fps, (width,height))
 
+###### Setup the detection model with preferable gpu usage ######
 net = cv2.dnn.readNet("darknet/yolov4.weights", "darknet/cfg/yolov4.cfg")
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
-
 model = cv2.dnn_DetectionModel(net)
 model.setInputParams(size=(416, 416), scale=1/255)
 
 
-class Person:
-    score: int
-    box: list
-    social_distancing: bool
-
-    def __init__(self, score, box):
-        self.score = score
-        self.box = box
-        self.social_distancing = True
-        self.ground_x = None
-        self.ground_y = None
-
-    def __eq__(self, other):
-        for a, b in zip(self.box, other.box):
-            if a != b:
-                return False
-        return True
-    
-    def set_ground_coordinates(self, M):
-        pt = np.append(self.feet, [1])
-        mapped_pt = np.dot(M, pt)
-        self.ground_y = int(mapped_pt[0] / mapped_pt[2])
-        self.ground_x = int(mapped_pt[1] / mapped_pt[2])
-
-    @property
-    def middle(self):
-        # middle of the box 
-        return (self.box[0] + self.box[2]//2, self.box[1] + self.box[3]//2)
-
-    @property
-    def feet(self):
-        # bottom middle of the box (ie where the persons feet are in (col, row)/(x,y) )
-        return (self.box[0] + self.box[2]//2, self.box[1] + self.box[3])
-
-    @property
-    def ground_coordinates(self):
-        # bottom middle of the box (ie where the persons feet are in (col, row)/(x,y) )
-        return (self.ground_x, self.ground_y)
-
-    @property
-    def width(self):
-        return self.box[1]
-
-    @property
-    def height(self):
-        return self.box[0]
-
-    def get_distance_between_persons(self, other):
-        dist = np.sqrt((self.ground_x - other.ground_x)**2 + (self.ground_y - other.ground_y)**2)
-        return dist
-
-    def set_social_distancing(self, other):
-        if self.get_distance_between_persons(other) < social_distancing_min_distance:
-            self.social_distancing = False
-
-        
-
-# Setup the video writer
-(grabbed, frame) = vc.read() #Steal the first frame to get the size hahaha
-height, width = frame.shape[0], frame.shape[1]
-fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
-video = cv2.VideoWriter(f'./{output_folder}/{file_name}_detection.avi', fourcc, fps, (width,height))
-
-# Fit the homography matrix mapping for the sample mall video
+###### Fit the homography matrix mapping for the sample mall video ######
 src_pts = np.array([[60, 153], [359, 153], [50, 201], [367, 200]])
 dst_pts = np.array([[0, 0], [0, 975], [382, 98], [382, 878]])
-floor_depth, floor_width =  1000, 1000
+floor_depth, floor_width =  1100, 1100
 M, mask = cv2.findHomography(src_pts, dst_pts, 0)
 
-# TEST THE HOMOGRAPHY
-new_img = np.zeros((floor_depth, floor_width, 3))
-
-for r in range(height):
-    for c in range(width):
-        x = c
-        y = r
-        pt = np.array([x,y,1])
-        mapped_pt = np.dot(M, pt)
-        mapped_r = int(mapped_pt[0] / mapped_pt[2])
-        mapped_c = int(mapped_pt[1] / mapped_pt[2])
-
-        if 0 <= mapped_c < floor_width and 0 <= mapped_r < floor_depth:
-            new_img[mapped_r, mapped_c] = frame[r,c]
-
-cv2.imwrite(f'{output_folder}/test_homography.jpg', new_img[:,:,::-1])
-
+###### Setup the video writer for the combined video detection + 2d floor location points ######
 ground_video = cv2.VideoWriter(f'./{output_folder}/{file_name}_ground.avi', fourcc, fps, (floor_width*2, floor_depth))
+
 
 frame_number = 0
 while vc.isOpened():
@@ -162,7 +93,7 @@ while vc.isOpened():
         cv2.putText(frame, label, person.middle, cv2.FONT_HERSHEY_SIMPLEX, 0.3, color, 2) # (box[0], box[1] - 10) (old text location)
 
         # draw a circle where the person is on the ground plane frame
-        cv2.circle(ground_plane, (person.ground_x, person.ground_y+600), 15, color, -1)
+        cv2.circle(ground_plane, (person.ground_x, person.ground_y+700), 15, color, -1)
     end_drawing = time.time()
     
     fps_label = "FPS: %.2f (excluding drawing time of %.2fms)" % (1 / (end - start), (end_drawing - start_drawing) * 1000)
@@ -174,7 +105,7 @@ while vc.isOpened():
     
     # Write the frame to the videowriter
     combined_frames = np.hstack((cv2.resize(frame, (floor_depth, floor_width)), ground_plane))
-    video.write(frame)
+    video_writer.write(frame)
     ground_video.write(combined_frames)
 
     # Show the frame with imshow, TODO: can change, doesn't work on wsl without extra software
